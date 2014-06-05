1f265ec44be96c8524b5e2432add07d88f2badd6
emchapple <emchapple@gmail.com>
2014-06-04 22:35:21 -0700

    Initial Commit

---
+++ Comp2614Assignment4/FormMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Comp2614Assignment4
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewAccountsDisplay = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonDeposit = new System.Windows.Forms.Button();
+            this.buttonWithdraw = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewAccountsDisplay
+            // 
+            this.listViewAccountsDisplay.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listViewAccountsDisplay.Location = new System.Drawing.Point(58, 46);
+            this.listViewAccountsDisplay.Name = "listViewAccountsDisplay";
+            this.listViewAccountsDisplay.Size = new System.Drawing.Size(727, 162);
+            this.listViewAccountsDisplay.TabIndex = 0;
+            this.listViewAccountsDisplay.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Account Name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Current Balance";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Credit Limit";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Available Funds";
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 5;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanelMain.Controls.Add(this.buttonDeposit, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonWithdraw, 2, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.button3, 4, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.button4, 4, 2);
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(58, 227);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 3;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(727, 111);
+            this.tableLayoutPanelMain.TabIndex = 1;
+            // 
+            // buttonDeposit
+            // 
+            this.buttonDeposit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonDeposit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDeposit.Location = new System.Drawing.Point(3, 3);
+            this.buttonDeposit.Name = "buttonDeposit";
+            this.buttonDeposit.Size = new System.Drawing.Size(175, 38);
+            this.buttonDeposit.TabIndex = 0;
+            this.buttonDeposit.Text = "Deposit";
+            this.buttonDeposit.UseVisualStyleBackColor = true;
+            // 
+            // buttonWithdraw
+            // 
+            this.buttonWithdraw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonWithdraw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonWithdraw.Location = new System.Drawing.Point(274, 3);
+            this.buttonWithdraw.Name = "buttonWithdraw";
+            this.buttonWithdraw.Size = new System.Drawing.Size(175, 38);
+            this.buttonWithdraw.TabIndex = 1;
+            this.buttonWithdraw.Text = "Withdraw";
+            this.buttonWithdraw.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(545, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(179, 38);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Transfer Funds";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(545, 69);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(179, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Transaction History";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(852, 358);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Controls.Add(this.listViewAccountsDisplay);
+            this.Name = "FormMain";
+            this.Text = "Accounts For: <Customer>";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewAccountsDisplay;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.Button buttonDeposit;
+        private System.Windows.Forms.Button buttonWithdraw;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        public System.Windows.Forms.ColumnHeader columnHeader1;
+    }
+}
+
